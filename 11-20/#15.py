4e848b72f9c2b549dff8424a49c6d4f73cfa2ef8
Minestone4306 <78621484+dcjvliet@users.noreply.github.com>
2024-07-10 14:27:04 -0400

    Delete 11-20 directory

--- 11-20/#15.py
+++
@@ -1,8 +0,0 @@
-import math
-
-
-def lattice_paths(num):
-    return math.factorial(2 * num) / (math.factorial(num)) ** 2
-
-
-print(lattice_paths(20))
--- 11-20/#16.py
+++
@@ -1,5 +0,0 @@
-num = 2 ** 1000
-sum = 0
-for char in str(num):
-    sum += int(char)
-print(sum)
