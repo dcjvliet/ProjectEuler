99beebf3f1289f9bfb44e40135520e506a01bbdb
Minestone4306 <78621484+dcjvliet@users.noreply.github.com>
2024-07-10 16:26:11 -0400

    Update #2.py

--- Shortest Solutions/#2.py
+++ Shortest Solutions/#2.py
@@ -1,6 +1,6 @@
-fibonacci = [1, 2]
-while fibonacci[-1] < 4000000:
-    fibonacci.append(fibonacci[-2] + fibonacci[-1])
-print(sum([fibonacci[i] for i in range(len(fibonacci)) if fibonacci[i] % 2 == 0]))
+f=[1,2]
+while f[-1]<4000000:
+    f.append(f[-2]+f[-1])
+print(sum([f[i] for i in range(len(f)) if f[i]%2==0]))
 
 # This is still a work in progress I know it can be shortened
