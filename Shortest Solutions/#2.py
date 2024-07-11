fa26ff7c7759ca8f9e25186784ce78e9d59ed0dc
Minestone4306 <78621484+dcjvliet@users.noreply.github.com>
2024-07-11 14:36:28 -0400

    Update #2.py

--- Shortest Solutions/#2.py
+++ Shortest Solutions/#2.py
@@ -1,2 +1,2 @@
-l = (lambda n,f=[1,1]:f if len(f)==n else (l(n,f+[f[-1]+f[-2]])))
+l=(lambda n,f=[1,1]:f if len(f)==n else (l(n,f+[f[-1]+f[-2]])))
 print(sum([n for n in l(200) if n%2==0 and n<4000000]))
