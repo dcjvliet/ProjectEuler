578bfe9e5f094cc347fb13321c63f8057faa464b
Minestone4306 <78621484+dcjvliet@users.noreply.github.com>
2024-07-11 13:21:36 -0400

    Update #2.py
    Still can be improved I think

--- Shortest Solutions/#2.py
+++ Shortest Solutions/#2.py
@@ -1,6 +1,2 @@
-f=[1,2]
-while f[-1]<4000000:
-    f.append(f[-2]+f[-1])
-print(sum([f[i] for i in range(len(f)) if f[i]%2==0]))
-
-# This is still a work in progress I know it can be shortened
+l = (lambda n,f=[1,1]:f if len(f)==n else (l(n,f+[f[-1]+f[-2]])))
+print(sum([n for n in l(200) if n%2==0 and n<4000000]))
